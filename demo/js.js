94b02dff4dd6563a12b01fb55a916232367b2220
Ahmad Awais <me@AhmadAwais.com>
2018-07-28 09:11:48 +0500

    👌 IMPROVE: Params

--- demo/js.js
+++ demo/js.js
@@ -35,5 +35,37 @@ function maedah(name) {
 	return name;
 }
 
+maedah('Maedah Batool');
+
 // Regex.
 const coursePlatformURL = new RegExp('/' + window.location.host + '/');
+
+import { btnBarSvgSpeed } from './constants';
+
+/**
+ *  Angle increment.
+ *
+ * — 360/(total speed values).
+ * — 360/6 = 60.
+ */
+export const speedAngles = {
+	'1': '0',
+	'1.25': '60',
+	'1.5': '120',
+	'1.75': '180',
+	'2': '240',
+	'0.75': '300'
+};
+
+/**
+ * Speed SVG CSS.
+ *
+ * @param Number Angle to rotate the SVG.
+ */
+export const speedCSS = (one, two, three) => {
+	btnBarSvgSpeed.css({
+		transform: 'rotate(' + one + 'deg)',
+		transform: 'rotate(' + two + 'deg)',
+		transform: 'rotate(' + three + 'deg)'
+	});
+};
