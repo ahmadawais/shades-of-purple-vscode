5b5dcf5d75cb4a3dc631624ec6d59998ba1a7e2b
Ahmad Awais <me@AhmadAwais.com>
2018-10-17 07:25:29 +0500

    📦 NEW: command line rainbow

--- demo/shellscript.sh
+++ demo/shellscript.sh
@@ -18,3 +18,9 @@ alias week='date +%V'
 
 
 ####.#### —————————————————— FUNCTIONS —————————————————— ####.####
+
+# Command line magic.
+function rainbow() {
+	yes "$(seq 231 -1 16)" | while read i; do printf "\x1b[48;5;${i}m\n"; sleep .02; done
+}
+
