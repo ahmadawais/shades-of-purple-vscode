d519b9c447b94f4f331ef842bc158313cb7251a0
Ahmad Awais <me@AhmadAwais.com>
2018-10-17 07:24:49 +0500

    📦 NEW: delete .DS_Store alias

--- demo/shellscript.sh
+++ demo/shellscript.sh
@@ -10,3 +10,6 @@ alias ...="cd ../../.."
 alias ....="cd ../../../.."
 alias cd..="cd .." # Typo addressed.
 
+# Recursively delete all `.DS_Store` files in the pwd.
+alias rmds="find . -type f -name '*.DS_Store' -ls -delete"
+
