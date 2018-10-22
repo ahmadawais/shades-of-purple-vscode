00cc8f93a50399c4173034de5c1a50b214a58fed
Ahmad Awais <me@AhmadAwais.com>
2018-10-22 13:19:58 +0500

    👌 IMPROVE: Show macOS Battery Percentage

--- demo/shellscript.sh
+++ demo/shellscript.sh
@@ -19,3 +19,7 @@ alias week='date +%V'
 function rainbow() {
 	yes "$(seq 231 -1 16)" | while read i; do printf "\x1b[48;5;${i}m\n"; sleep .02; done
 }
+
+function battery() {
+	pmset -g batt | egrep "([0-9]+\%).*" -o --colour=auto | cut -f1 -d';'
+}
