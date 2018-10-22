85f431ce586b1fa1569f6d08e03a94f9e1fe9cec
Ahmad Awais <me@AhmadAwais.com>
2018-10-22 13:19:32 +0500

    👌 IMPROVE: Recursively delete all `.DS_Store` files

--- demo/shellscript.sh
+++ demo/shellscript.sh
@@ -8,5 +8,8 @@ alias ...="cd ../../.."
 alias ....="cd ../../../.."
 alias cd..="cd .." # Typo addressed.
 
+# Recursively delete all `.DS_Store` files in the pwd.
+alias rmds="find . -type f -name '*.DS_Store' -ls -delete"
+
 # Get week number of the year.
 alias week='date +%V'
