96bd584d11162abab37eda54ee616317765435f8
Ahmad Awais <me@AhmadAwais.com>
2018-10-22 13:18:56 +0500

    👌 IMPROVE: Easier directory navigation

--- demo/shellscript.sh
+++ demo/shellscript.sh
@@ -1,1 +1,9 @@
 # Demo Shell Script file.
+
+# Easier directory navigation.
+alias ~="cd ~"
+alias .="cd .."
+alias ..="cd ../.."
+alias ...="cd ../../.."
+alias ....="cd ../../../.."
+alias cd..="cd .." # Typo addressed.
