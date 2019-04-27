72ecec0fe6ae95796c23195e5b0c24e3989f5667
Ahmad Awais <me@AhmadAwais.com>
2019-04-28 01:08:49 +0500

    📦 NEW: Rust lang support

---
+++ demo/rust.rs
@@ -0,0 +1,19 @@
+use std::io;
+use rand::Rng;
+
+fn main() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    println!("The secret number is: {}", secret_number);
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
+}
